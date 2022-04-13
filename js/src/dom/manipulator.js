zzz6519003/bootstrap

Manipulator: Add JSON parse support

js/src/dom/manipulator.js
@@ -22,7 +22,15 @@ function normalizeData(val) {
     return null
   }
 
-  return val
+  if (typeof val !== 'string') {
+    return val
+  }
+
+  try {
+    return JSON.parse(decodeURIComponent(val))
+  } catch {
+    return val
+  }
 }
 
 function normalizeDataKey(key) {

j-f1/forked-bootstrap

js/src/dom/manipulator.js
@@ -22,7 +22,15 @@ function normalizeData(value) {
     return null
   }
 
-  return value
+  if (typeof value !== 'string') {
+    return value
+  }
+
+  try {
+    return JSON.parse(decodeURIComponent(val))
+  } catch {
+    return value
+  }
 }
 
 function normalizeDataKey(key) {